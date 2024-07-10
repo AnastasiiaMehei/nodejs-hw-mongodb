2025a974d387f7bab27e82f4a638ed44eac60a14
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-07-10 20:04:35 +0300

    add

---
+++ src/db/models/contacts.js
@@ -0,0 +1,32 @@
+import { Schema } from 'mongoose';
+
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      optional: '',
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      required: true,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
