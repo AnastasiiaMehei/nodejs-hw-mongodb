bb4c7d618ae0391d6600a47b8446d7d1edfadf57
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-07-11 18:44:34 +0300

    add

--- src/server.js
+++ src/server.js
@@ -11,9 +11,13 @@ const PORT = env('PORT', '3003');
 
 export function setupServer() {
   const app = express();
-  app.use(express.json());
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+      limit: '100kb',
+    }),
+  );
   app.use(cors());
-
   app.use(
     pino({
       transport: {
