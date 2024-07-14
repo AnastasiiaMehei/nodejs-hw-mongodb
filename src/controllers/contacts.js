ffbd485ba5bb4d5ff20a98e4e15a52874acfe774
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-07-14 10:19:55 +0300

    add

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -3,8 +3,8 @@ import {
   createContact,
   deleteContact,
   updateContact,
+  getAllContacts,
 } from '../services/contacts.js';
-import { getAllContacts } from '../services/contacts.js';
 
 import createHttpError from 'http-errors';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
